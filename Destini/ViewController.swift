3f329d105149d3354fd9eced90044cfc376d18a6
Yusuf Çağlar <faf7faf7@gmail.com>
2019-10-25 14:35:12 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Yusuf ÇAĞLAR on 25/10/2019.
+//  Copyright © 2019 Yusuf ÇAĞLAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
