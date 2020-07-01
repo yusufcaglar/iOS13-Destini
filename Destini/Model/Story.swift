809d7d64c3190c4aa884645eb4389fafd6a5f732
Yusuf Çağlar <faf7faf7@gmail.com>
2020-07-01 21:36:03 +0300

    Write structures

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,18 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Yusuf ÇAĞLAR on 25/10/2019.
+//  Copyright © 2019 Yusuf ÇAĞLAR. All rights reserved.
+//
+
+import Foundation
+
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
